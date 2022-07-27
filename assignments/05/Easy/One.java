0c4908e85b49edae013af6940c16039eec87c9bc
Akash Wendole <akash.wendole3020@gmail.com>
2022-07-27 15:53:59 +0530

    Fourth Done

---
+++ assignments/05/Easy/One.java
@@ -0,0 +1,12 @@
+// Build Array from Permutation
+
+import java.util.Scanner;
+
+class One {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+    
+}
